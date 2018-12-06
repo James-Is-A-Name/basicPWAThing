61a4cd78f70929445206eccf82b8e4d92956ad9f
Karry <hello@devacademy.co.nz>
2018-12-06 15:52:49 +1300

    heroku port thing

--- index.js
+++ index.js
@@ -2,48 +2,48 @@ let express = require('express')
 
 let server = express()
 
-let port = 3000
+let port = process.env.PORT || 3000
 
 let someData = 0;
 
-server.get('/',(req,res) => {
-    res.sendFile(__dirname+'/pages/index.html')
+server.get('/', (req, res) => {
+    res.sendFile(__dirname + '/pages/index.html')
 })
 
 
 //things needed for the pwa
-server.get('/images/icons/icon-256x256.png',(req,res) => {
-    res.sendFile(__dirname+'/images/256.png')
+server.get('/images/icons/icon-256x256.png', (req, res) => {
+    res.sendFile(__dirname + '/images/256.png')
 })
-server.get('/images/icons/icon-128x128.png',(req,res) => {
-    res.sendFile(__dirname+'/images/128.png')
+server.get('/images/icons/icon-128x128.png', (req, res) => {
+    res.sendFile(__dirname + '/images/128.png')
 })
 
 //pwa files
-server.get('/manifest.json',(req,res) => {
-    res.sendFile(__dirname+'/manifest.json')
+server.get('/manifest.json', (req, res) => {
+    res.sendFile(__dirname + '/manifest.json')
 })
-server.get('/service-worker.js',(req,res) => {
-    res.sendFile(__dirname+'/service-worker.js')
+server.get('/service-worker.js', (req, res) => {
+    res.sendFile(__dirname + '/service-worker.js')
 })
 
 
 
 //just some html pages
-server.get('/other1',(req,res) => {
-    res.sendFile(__dirname+'/pages/other1.html')
+server.get('/other1', (req, res) => {
+    res.sendFile(__dirname + '/pages/other1.html')
 })
-server.get('/other2',(req,res) => {
-    res.sendFile(__dirname+'/pages/other2.html')
+server.get('/other2', (req, res) => {
+    res.sendFile(__dirname + '/pages/other2.html')
 })
 
 
-server.get('/data',(req,res) => {
+server.get('/data', (req, res) => {
     someData++;
-    res.send(""+someData);
+    res.send("" + someData);
 })
 
 
-server.listen( port,() => {
-    console.log('server up and running on port '+port)
+server.listen(port, () => {
+    console.log('server up and running on port ' + port)
 })
